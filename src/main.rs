0effd83157fad3adbe4a23b6a1ff1ed6968bb62d
Adrian Wong <adrianwjw@gmail.com>
2020-01-18 22:53:54 +1100

    Specify ROM file path via command line arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod chip8;
+
+use std::env;
+use std::io;
+
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "ROM file not specified. Usage: ./chip8 [rom_file]",
+        ));
+    }
+
+    Ok(())
 }
